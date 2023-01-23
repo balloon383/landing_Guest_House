f3f316d4d63f1b9b4188a1335f23b6bdd301f439
Anton Chekalenko <aston23112001@gmail.com>
2023-01-23 21:29:39 +0200

    responses block added, swiper done

--- js/main.js
+++ js/main.js
@@ -2,18 +2,10 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
 direction: 'horizontal',
     loop: true,
-    spaceBetween: 30,
+    spaceBetween: 20,
   // If we need pagination
-pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    },
-    slidesPerView: 1, 
-    /* breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 4,
-            spaceBetween: 20
-        },
-    } */
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
