5befe06bbe1bc38cf8f134e14608dbbc1c09327d
Anton Chekalenko <aston23112001@gmail.com>
2023-01-28 13:00:49 +0200

    lesson 2 changes, prompt results in console log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
 direction: 'horizontal',
@@ -9,3 +10,8 @@ navigation: {
     prevEl: '.swiper-button-prev',
   },
 });
+const yourAge = prompt("how old are you?")
+const yourName = prompt("what is your name?")
+const yourSecondName = prompt("what is your second name?")
+
+console.log("User bio: user first name is " + yourName + ", user second name is " + yourSecondName + ", user age of birth is " + (2023 - +yourAge))
