814a48a37530f175a75da734721af47f249279ad
Anton Chekalenko <aston23112001@gmail.com>
2023-01-31 23:52:39 +0200

    added zodiac counter

--- js/main.js
+++ js/main.js
@@ -10,8 +10,38 @@ navigation: {
     prevEl: '.swiper-button-prev',
   },
 });
-const yourAge = prompt("how old are you?")
-const yourName = prompt("what is your name?")
-const yourSecondName = prompt("what is your second name?")
+const yourName = prompt('What is your name?');
+const yourSecondName = prompt('What is your second name?');
+const yourDayOfBirth = +prompt('What is your day of birth');
+const yourMonthOfBirth = +prompt('What is your month of birth');
+const yourYearOfBirth = 2023 - +prompt('What is your year of birth?');
+let yourZodiac
+if (yourDayOfBirth > 31 || yourMonthOfBirth > 12) {
+  yourZodiac = 'Wrong day or month of birth'
+} else if ((yourDayOfBirth >= 20 && yourMonthOfBirth == 1) || (yourDayOfBirth <= 18 && yourMonthOfBirth == 2)) {
+    yourZodiac = 'Aquaris';
+} else if ((yourDayOfBirth >= 19 && yourMonthOfBirth == 2) || (yourDayOfBirth <= 20 && yourMonthOfBirth == 3)){
+    yourZodiac = 'Pisces';
+} else if ((yourDayOfBirth >= 21 && yourMonthOfBirth == 3) || (yourDayOfBirth <= 19 && yourMonthOfBirth == 4)){
+  yourZodiac = 'Aries';
+} else if ((yourDayOfBirth >= 20 && yourMonthOfBirth == 4) || (yourDayOfBirth <= 20 && yourMonthOfBirth == 5)){
+  yourZodiac = 'Taurus';
+} else if ((yourDayOfBirth >= 21 && yourMonthOfBirth == 5) || (yourDayOfBirth <= 20 && yourMonthOfBirth == 6)){
+  yourZodiac = 'Gemini';
+} else if ((yourDayOfBirth >= 21 && yourMonthOfBirth == 6) || (yourDayOfBirth <= 22 && yourMonthOfBirth == 7)){
+  yourZodiac = 'Cancer';
+} else if ((yourDayOfBirth >= 23 && yourMonthOfBirth == 7) || (yourDayOfBirth <= 22 && yourMonthOfBirth == 8)){
+  yourZodiac = 'Leo';
+} else if ((yourDayOfBirth >= 23 && yourMonthOfBirth == 8) || (yourDayOfBirth <= 22 && yourMonthOfBirth == 9)){
+  yourZodiac = 'Virgo';
+} else if ((yourDayOfBirth >= 23 && yourMonthOfBirth == 9) || (yourDayOfBirth <= 22 && yourMonthOfBirth == 10)){
+  yourZodiac = 'Libra';
+} else if ((yourDayOfBirth >= 21 && yourMonthOfBirth == 10) || (yourDayOfBirth <= 22 && yourMonthOfBirth == 11)){
+  yourZodiac = 'Scorpio';
+} else if ((yourDayOfBirth >= 21 && yourMonthOfBirth == 11) || (yourDayOfBirth <= 22 && yourMonthOfBirth == 12)){
+  yourZodiac = 'Saggitarius';
+} else if ((yourDayOfBirth >= 23 && yourMonthOfBirth == 12) || (yourDayOfBirth <= 19 && yourMonthOfBirth == 1)){
+  yourZodiac = 'Capricorn';
+}
 
-console.log("User bio: user first name is " + yourName + ", user second name is " + yourSecondName + ", user age of birth is " + (2023 - +yourAge))
+console.log(yourName, yourSecondName, yourDayOfBirth, yourMonthOfBirth, yourYearOfBirth, yourZodiac)
